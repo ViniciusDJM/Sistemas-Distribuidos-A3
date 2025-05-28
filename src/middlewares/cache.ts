1e63ebc546695df833e53397a74fbf7c4df6ae46
Vinícius Maciel <vinyfernandes10@hotmail.com>
2025-05-28 20:15:54 -0300

    fix: cache problem in data argument

--- src/middlewares/cache.ts
+++ src/middlewares/cache.ts
@@ -5,22 +5,26 @@ export function cacheMiddleware(req: Request, res: Response, next: NextFunction)
   const originalSend = res.send;
 
   res.send = function (body): Response {
-    const etag = crypto.createHash('md5').update(JSON.stringify(body)).digest('hex');
+    if (body !== undefined) {
+      const etag = crypto.createHash('md5').update(JSON.stringify(body)).digest('hex');
 
-    res.setHeader('ETag', `"${etag}"`);
-    res.setHeader('Last-Modified', new Date().toUTCString());
-    res.setHeader('Cache-Control', 'public, max-age=60');
+      res.setHeader('ETag', `"${etag}"`);
+      res.setHeader('Last-Modified', new Date().toUTCString());
+      res.setHeader('Cache-Control', 'public, max-age=60');
 
-    const ifNoneMatch = req.headers['if-none-match'];
-    if (ifNoneMatch === `"${etag}"`) {
-      res.status(304).send();
-      return res;
-    }
+      const ifNoneMatch = req.headers['if-none-match'];
+      if (ifNoneMatch === `"${etag}"`) {
+        res.status(304).send();
+        return res;
+      }
 
-    const ifModifiedSince = req.headers['if-modified-since'];
-    if (ifModifiedSince && new Date(ifModifiedSince) >= new Date()) {
-      res.status(304).send();
-      return res;
+      const ifModifiedSince = req.headers['if-modified-since'];
+      if (ifModifiedSince && new Date(ifModifiedSince) >= new Date()) {
+        res.status(304).send();
+        return res;
+      }
+    } else {
+      console.warn('⚠️ res.send recebeu body undefined no cacheMiddleware');
     }
 
     return originalSend.call(this, body);
